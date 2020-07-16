9b7d08c7a1128d1152f18b20670f9f13805439c7
quoc1405 <quocnguyen1405@gmail.com>
2020-07-16 09:30:47 +0700

    update

--- 2. Thiết kế và tạo CSDL/Bài Tập/Xác Định Khóa chính Khóa Ngoại của bảng/xacdinhkhoachinhkhoangoai.sql
+++ 2. Thiết kế và tạo CSDL/Bài Tập/Xác Định Khóa chính Khóa Ngoại của bảng/xacdinhkhoachinhkhoangoai.sql
@@ -2,18 +2,18 @@ create database bank_management;
 use bank_management;
 
 create table Customer(
-customer_number int primary key not null unique,
+customer_number int primary key ,
 full_name varchar(40),
 address varchar(40),
 email varchar(40),
 phone  int(10)
 );
 create table TypeAcount(
-idAccount int not null unique primary key,
+idAccount int  primary key,
 `name` varchar(40)
 );
 create table Accounts(
-account_number int primary key not null unique,
+account_number int  unique,
 account_type  varchar(40),
 `date` datetime,
 balance double,
@@ -24,7 +24,7 @@ foreign key(id_typeOfAccount) references TypeAcount(idAccount)
 );
 
 create table Transactions (
-tran_number int primary key not null unique,
+tran_number int primary key ,
 account_number int,
 `date` datetime,
 number_account int,
